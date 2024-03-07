0c9b9e7fd9595eff54e9ae2a8b9e0cc23491c388
Gideon5 <abaaneg@gmail.com>
2024-03-07 19:49:28 +0000

    welcomeScreen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  var height, width;
+
+  @override
+  Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: height,
+            width: width,
+            color: Colors.grey.shade400,
+            child: Image.asset(
+              "assets/images/s.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              height: height * 0.32,
+              width: width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "Let's start Journey, Enjoy",
+                    style: TextStyle(
+                        fontSize: width * 0.058,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white),
+                  ),
+                  Text(
+                    "Beautiful Movement of life",
+                    style: TextStyle(
+                        fontSize: width * 0.058,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: height * 0.02,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      height: height * 0.065,
+                      width: width * 0.75,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.white),
+                      child: Center(
+                        child: Text(
+                          "Start",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: width * 0.04,
+                              fontWeight: FontWeight.w800,
+                              letterSpacing: 1),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: height * 0.02),
+                  Text("Don't have an account", style: TextStyle(
+                    fontSize: width * 0.04,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white
+                  ),),
+                  SizedBox(height: height * 0.02,),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
